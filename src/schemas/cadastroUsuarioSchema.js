9d42856bae265b1c61f9d4c03c7bf64a76023754
Eduardo Lago Nunes <eduardolagonunes@gmail.com>
2024-02-06 09:43:06 -0300

    testando mensagens de validações agora vai!

--- src/schemas/cadastroUsuarioSchema.js
+++ src/schemas/cadastroUsuarioSchema.js
@@ -13,37 +13,35 @@ const cadastroUsuarioSchema = joi.object({
     "string.base": "Insira um e-mail válido",
     "string.trim": "O campo e-mail não pode conter espaços em branco",
   }),
-  senha: joi
-    .string()
+  senha: Joi.string()
     .required()
     .min(8)
     .pattern(
-      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%?&])[0-9a-zA-Z@$!%?&]{8,}$/
+        new RegExp(/^(?=.*[a-z])/),
+        { name: 'lowercaseLetters', invert: false }
+    )
+    .pattern(
+        new RegExp(/^(?=.*[A-Z])/),
+        { name: 'uppercaseLetters', invert: false }
+    )
+    .pattern(
+        new RegExp(/^(?=.*\d)/),
+        { name: 'numbers', invert: false }
+    )
+    .pattern(
+        new RegExp(/^(?=.*[@$!%?&])/),
+        { name: 'specialCharacters', invert: false }
     )
     .messages({
-      "any.required": "O campo senha é obrigatório",
-      "string.empty": "O campo senha é obrigatório",
-      "string.min": "A senha deve ter no mínimo 8 caracteres",
-      "string.trim": "O campo senha não pode conter espaços em branco",
-      "string.base": "Insira uma senha válida",
-      "string.pattern.invert": [
-        {
-          message: "A senha deve conter pelo menos 1 número",
-          regexp: /^(?=.*\d)/,
-        },
-        {
-          message: "A senha deve conter pelo menos 1 letra minúscula",
-          regexp: /^(?=.*[a-z])/,
-        },
-        {
-          message: "A senha deve conter pelo menos 1 letra maiúscula",
-          regexp: /^(?=.*[A-Z])/,
-        },
-        {
-          message: "A senha deve conter pelo menos 1 caractere especial",
-          regexp: /^(?=.*[@$!%?&])/,
-        },
-      ],
+        "any.required": "O campo senha é obrigatório",
+        "string.empty": "O campo senha é obrigatório",
+        "string.min": "A senha deve ter no mínimo 8 caracteres",
+        "string.trim": "O campo senha não pode conter espaços em branco",
+        "string.base": "Insira uma senha válida",
+        "lowercaseLetters.base": "A senha deve conter pelo menos 1 letra minúscula",
+        "uppercaseLetters.base": "A senha deve conter pelo menos 1 letra maiúscula",
+        "numbers.base": "A senha deve conter pelo menos 1 número",
+        "specialCharacters.base": "A senha deve conter pelo menos 1 caractere especial"
     }),
   tipoCadastro: joi.required().messages({
     "any.required": "O campo tipo de cadastro deve ser selecionado",
