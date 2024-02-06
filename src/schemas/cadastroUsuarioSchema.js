ebc0ccbce377c86856d25b3677feeb3e123b2e45
Eduardo Lago Nunes <eduardolagonunes@gmail.com>
2024-02-06 09:19:26 -0300

    testando mensagens de validações parte 2

--- src/schemas/cadastroUsuarioSchema.js
+++ src/schemas/cadastroUsuarioSchema.js
@@ -26,19 +26,28 @@ const cadastroUsuarioSchema = joi.object({
       "string.min": "A senha deve ter no mínimo 8 caracteres",
       "string.trim": "O campo senha não pode conter espaços em branco",
       "string.base": "Insira uma senha válida",
-      /* "string.pattern.base":
-        "A senha deve conter pelo menos 1 letra maiúscula, 1 número e 1 dos seguintes símbolos @,$,!,%,? ou &", */
     })
-    .messages({
-      "string.pattern.base": 
-          "A senha deve conter pelo menos 1 letra minúscula",
-      "string.pattern.base": 
-          "A senha deve conter pelo menos 1 letra maiúscula",
-      "string.pattern.base": 
-          "A senha deve conter pelo menos 1 número",
-      "string.pattern.base": 
+    .custom((value, helpers) => {
+      if (!/(?=.*[a-z])/.test(value)) {
+        return helpers.message(
+          "A senha deve conter pelo menos 1 letra minúscula"
+        );
+      }
+      if (!/(?=.*[A-Z])/.test(value)) {
+        return helpers.message(
+          "A senha deve conter pelo menos 1 letra maiúscula"
+        );
+      }
+      if (!/(?=.*\d)/.test(value)) {
+        return helpers.message("A senha deve conter pelo menos 1 número");
+      }
+      if (!/(?=.*[!@#\$%\^&\*\(\)_\-=+'\[{\]};:'<,>.?\/\\])/.test(value)) {
+        return helpers.message(
           "A senha deve conter pelo menos 1 caractere especial"
-  }),
+        );
+      }
+      return value;
+    }),
   tipoCadastro: joi.required().messages({
     "any.required": "O campo tipo de cadastro deve ser selecionado",
   }),
