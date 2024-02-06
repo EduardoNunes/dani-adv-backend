a003289c8088fca1e55570242a46b1147ab904bd
Eduardo Lago Nunes <eduardolagonunes@gmail.com>
2024-02-06 09:39:26 -0300

    testando mensagens de validações mais uma tentativa 2

--- src/schemas/cadastroUsuarioSchema.js
+++ src/schemas/cadastroUsuarioSchema.js
@@ -26,8 +26,6 @@ const cadastroUsuarioSchema = joi.object({
       "string.min": "A senha deve ter no mínimo 8 caracteres",
       "string.trim": "O campo senha não pode conter espaços em branco",
       "string.base": "Insira uma senha válida",
-      "string.pattern.base":
-        "A senha deve conter pelo menos 1 letra maiúscula, 1 número e 1 caractere especial",
       "string.pattern.invert": [
         {
           message: "A senha deve conter pelo menos 1 número",
