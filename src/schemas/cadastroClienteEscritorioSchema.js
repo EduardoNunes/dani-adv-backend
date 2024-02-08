ceca7c8d7e5c35a673c176b5d50cb8d4d113ff60
Eduardo Lago Nunes <eduardolagonunes@gmail.com>
2024-02-08 15:42:48 -0300

    alguns joi do schema cadastro cliente escritorio feitos

--- src/schemas/cadastroClienteEscritorioSchema.js
+++ src/schemas/cadastroClienteEscritorioSchema.js
@@ -1,23 +1,57 @@
 const joi = require("joi");
 
 const cadastroClienteEscritorioSchema = joi.object({
-  nome: joi.string(),
+  nome: joi.string().min(5).required().messages({
+    "string.empty": "O campo nome é obrigatório",
+    "any.required": "O campo nome é obrigatório",
+    "string.base": "Insira um nome válido",
+  }),
   nascimento: joi.string().allow(null, ''),
   genero: joi.string().allow(null, ''),
   nacionalidade: joi.string().allow(null, ''),
-  celular: joi.string(),
-  email: joi.string(),
+  celular: joi.string().required().messages({
+    "any.required": "O campo celular é obrigatório",
+    "string.empty": "O campo celular é obrigatório",
+  }),
+  email: joi.string().email().required().messages({
+    "any.required": "O campo email é obrigatório",
+    "string.empty": "O campo email é obrigatório",
+    "string.email": "Digite um e-mail válido",
+    "string.base": "Insira um e-mail válido",
+    "string.trim": "O campo e-mail não pode conter espaços em branco",
+  }),
   redes_sociais: joi.string().allow(null, ''),
-  rg: joi.string(),
-  cpf: joi.string(),
+  rg: joi.string().required().messages({
+    "any.required": "O campo RG é obrigatório",
+    "string.empty": "O campo RG é obrigatório",
+  }),
+  cpf: joi.string().required().messages({
+    "any.required": "O campo CPF é obrigatório",
+    "string.empty": "O campo CPF é obrigatório",
+  }),
   profissao: joi.string().allow(null, ''),
   estado_civil: joi.string().allow(null, ''),
   formacao_academica: joi.string().allow(null, ''),
-  cep: joi.string(),
-  cidade: joi.string(),
-  bairro: joi.string(),
-  uf: joi.string(),
-  logradouro: joi.string(),
+  cep: joi.string().required().messages({
+    "any.required": "O campo CEP é obrigatório",
+    "string.empty": "O campo CEP é obrigatório",
+  }),
+  cidade: joi.string().required().messages({
+    "any.required": "O campo cidade é obrigatório",
+    "string.empty": "O campo cidade é obrigatório",
+  }),
+  bairro: joi.string().required().messages({
+    "any.required": "O campo bairro é obrigatório",
+    "string.empty": "O campo bairro é obrigatório",
+  }),
+  uf: joi.string().required().messages({
+    "any.required": "O campo UF é obrigatório",
+    "string.empty": "O campo UF é obrigatório",
+  }),
+  logradouro: joi.string().required().messages({
+    "any.required": "O campo logradouro é obrigatório",
+    "string.empty": "O campo logradouro é obrigatório",
+  }),
   complemento: joi.string().allow(null, ''),
   infos: joi.string().allow(null, ''),
 });
